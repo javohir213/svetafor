31d67233505ca38b1df71f8c4dd5188edd68d54a
javohir213 <ajavohir213@gmail.com>
2022-04-11 13:08:21 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// const elModal=document.querySelector("modal");
+
+// elModal.addEventListener("click",(evt)=>{
+//     const isElRemover=evt.target.matches(".moda")
+// })
+
+// const apple = {
+//   founder: "Steve Jobs",
+//   ceo: "Tim Cook",
+//   copitalezation: "$3 trillion",
+//   reseasedDate: "June 29,2007",
+//   products: [
+//     {
+//       id: 1,
+//       productName: "iWatch 7",
+//       price: 400,
+//       currency: "$",
+//     },
+//     {
+//       id: 2,
+//       productName: "iWatch 7",
+//       price: 400,
+//       currency: "$",
+//     },
+//     {
+//       id: 3,
+//       productName: "iWatch 7",
+//       price: 400,
+//       currency: "$",
+//     },
+//   ],
+// };
+// console.log(apple.founder);
